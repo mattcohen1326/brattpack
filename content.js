fa525c70a1d53af86d06b308a39b08878105902c
mattcohen1326 <maco2574@colorado.edu>
2022-07-05 18:55:51 -0400

    dynamicChangesUpdate

---
+++ content.js
@@ -0,0 +1,45 @@
+
+var myJSON = {
+    "content" : [
+        {
+            "type":"space",
+            "title":"Devils Fam Twitter Space July 7th",
+            "date":"77",
+            "route":"spaces.html#77",
+            "img":"img/spaces/77.jpg"
+        },
+        {
+            "type":"space",
+            "title":"Devils Fam Twitter Space July 10th",
+            "date":"710",
+            "route":"spaces.html#710",
+            "img":"img/spaces/710.jpg"
+        },
+        {
+            "type":"appearance",
+            "title":"Devils State Of Mind",
+            "route":"Appearances.html",
+            "img":"img/3.png"
+        },
+        {
+            "type":"space",
+            "title":"Devils Fam Twitter Space July 13th",
+            "date":"713",
+            "route":"spaces.html#713",
+            "img":"img/spaces/713.jpg"
+        },
+        {
+            "type":"appearance",
+            "title":"Locked On Devils Podcast",
+            "route":"Appearances.html",
+            "img":"img/1.png"
+        },
+        {
+            "type":"appearance",
+            "title":"Ice Cold Takes With The Boys In Blue",
+            "route":"Appearances.html",
+            "img":"img/2.png"
+        }
+        
+    ]
+}
